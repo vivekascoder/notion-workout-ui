88208771920900325d93ff134cc67d5e23c977ff
vivekascoder <vivekascoder@gmail.com>
2025-03-26 02:07:17 +0530

    fix: margin issue

--- src/app/components/notion-graph-ui.tsx
+++ src/app/components/notion-graph-ui.tsx
@@ -282,7 +282,7 @@ export default function NotionGraphUi() {
   }, []);
 
   return (
-    <div className="space-y-10 md:mx-0 px-10">
+    <div className="space-y-10 md:mx-0 px-2">
       <div>
         <h2 className="text-xl font-semibold mb-5">Body weight</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,57 +10,15 @@ export default function Home() {
   return (
     <div className="max-w-4xl mx-auto">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex items-center justify-between w-full py-5 md:px-0 px-10">
+        <div className="flex items-center justify-between w-full py-5 md:px-0 px-2">
           <h1 className="text-3xl font-semibold">📊 Notion workout logs UI</h1>
           <ModeToggle />
         </div>
         <NotionGraphUi />
       </main>
       <footer className="mt-10 row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+        <a href="https://vivek.ink" className="m-2">
+          Made by: @vivek
         </a>
       </footer>
     </div>
